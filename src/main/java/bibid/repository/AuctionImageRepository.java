7c0a7a70199f85ba3c424cdd6cd5cd05135e93d1
dydhyun <dydgus625@naver.com>
2024-10-25 15:50:26 +0900

    tmp

--- src/main/java/bibid/repository/AuctionImageRepository.java
+++ src/main/java/bibid/repository/AuctionImageRepository.java
@@ -4,9 +4,8 @@ import bibid.entity.AuctionImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface auctionImageRepository extends JpaRepository<AuctionImage, Long> {
+public interface AuctionImageRepository extends JpaRepository<AuctionImage, Long> {
 
     List<AuctionImage> findByAuction_AuctionIndex(Long auctionIndex);
 
