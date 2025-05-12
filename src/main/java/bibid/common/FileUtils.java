d697e26c9476eaa08b5a9854c37c1b1fd086d8d3
Rurouni98 <byungju98@gmail.com>
2025-05-12 19:57:59 +0900

    05.12 네이버 클라우드 및 네이버 오브젝트 스토리지 관련 설정 변경, livestation 및 auth 설정 변경 필요

--- src/main/java/bibid/common/FileUtils.java
+++ src/main/java/bibid/common/FileUtils.java
@@ -1,24 +1,15 @@
 package bibid.common;
 
-import bibid.config.NaverConfiguration;
 import bibid.dto.AuctionImageDto;
 import bibid.dto.ProfileImageDto;
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.CannedAccessControlList;
-import com.amazonaws.services.s3.model.ObjectMetadata;
-import com.amazonaws.services.s3.model.PutObjectRequest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -27,146 +18,79 @@ import java.util.UUID;
 public class FileUtils {
 
     @Value("${cloud.aws.s3.bucket.name}")
-    String bucket;
+    private String bucket;
 
-    private final AmazonS3 s3;
+    private final S3Client s3;
 
-    public FileUtils(NaverConfiguration naverConfiguration) {
-        s3 = AmazonS3ClientBuilder.standard()
-                .withEndpointConfiguration(
-                        new AwsClientBuilder.EndpointConfiguration(
-                                naverConfiguration.getEndPoint(),
-                                naverConfiguration.getRegionName()
-                        )
-                )
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicAWSCredentials(
-                                        naverConfiguration.getAccessKey(),
-                                        naverConfiguration.getSecretKey()
-                                )
-                        )
-                )
-                .build();
+    public FileUtils(S3Client s3Client) {
+        this.s3 = s3Client;
     }
 
     public ProfileImageDto parserFileInfo(MultipartFile multipartFile, String directory) {
-        String bucketName = bucket;
-
-        ProfileImageDto profileImageDto = new ProfileImageDto();
-
-        // 다른 사용자가 같은 파일명의 파일을 업로드 했을 때
-        // 덮어써지는 것을 방지하기 위해서 파일명을 랜덤값_날짜시간_파일명으로 지정
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-        Date nowDate = new Date();
-
-        String nowDateStr = format.format(nowDate);
-
-        UUID uuid = UUID.randomUUID();
-
-        String fileName =  uuid.toString() + "_" + nowDateStr + "_" + multipartFile.getOriginalFilename();
-
-        // Object Storage에 파일 업로드
-        try(InputStream fileInputStream = multipartFile.getInputStream()) {
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(multipartFile.getContentType());
-
-            PutObjectRequest putObjectRequest = new PutObjectRequest(
-                    bucketName,
-                    directory + fileName,
-                    fileInputStream,
-                    objectMetadata
-            ).withCannedAcl(CannedAccessControlList.PublicRead);
-
-            s3.putObject(putObjectRequest);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        File uploadFile = new File(directory + fileName);
-        String type = "";
+        String now = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String uuid = UUID.randomUUID().toString();
+        String originalName = multipartFile.getOriginalFilename();
+        String fileName = uuid + "_" + now + "_" + originalName;
+        String key = directory + fileName;
 
         try {
-            type = Files.probeContentType(uploadFile.toPath());
-        } catch(IOException ie) {
-            System.out.println(ie.getMessage());
+            PutObjectRequest putRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(multipartFile.getContentType())
+                    .acl("public-read") // R2에서 지원 가능, Cloudflare에 따라 무시될 수도 있음
+                    .build();
+
+            s3.putObject(putRequest, RequestBody.fromBytes(multipartFile.getBytes()));
+
+        } catch (IOException e) {
+            System.out.println("파일 업로드 실패: " + e.getMessage());
         }
 
-        if(!type.equals("")) {
-            if(type.startsWith("image")) {
-                profileImageDto.setFiletype("image");
-            } else {
-                profileImageDto.setFiletype("etc");
-            }
-        } else {
-            profileImageDto.setFiletype("etc");
-        }
-
-        profileImageDto.setNewfilename(fileName);
-        profileImageDto.setFilesize(multipartFile.getSize());
-        profileImageDto.setOriginalname(multipartFile.getOriginalFilename());
-        profileImageDto.setFilepath(directory);
-
-        return profileImageDto;
+        ProfileImageDto dto = new ProfileImageDto();
+        dto.setNewfilename(fileName);
+        dto.setOriginalname(originalName);
+        dto.setFilesize(multipartFile.getSize());
+        dto.setFilepath(directory);
+        dto.setFiletype(
+                multipartFile.getContentType() != null && multipartFile.getContentType().startsWith("image")
+                        ? "image"
+                        : "etc"
+        );
+        return dto;
     }
 
     public AuctionImageDto auctionImageParserFileInfo(MultipartFile multipartFile, String directory) {
-        String bucketName = bucket;
-
-        AuctionImageDto auctionImageDto = new AuctionImageDto();
-
-        // 다른 사용자가 같은 파일명의 파일을 업로드 했을 때
-        // 덮어써지는 것을 방지하기 위해서 파일명을 랜덤값_날짜시간_파일명으로 지정
-        SimpleDateFormat format = new SimpleDateFormat("yyyyMMddHHmmss");
-        Date nowDate = new Date();
-
-        String nowDateStr = format.format(nowDate);
-
-        UUID uuid = UUID.randomUUID();
-
-        String fileName =  uuid.toString() + "_" + nowDateStr + "_" + multipartFile.getOriginalFilename();
-
-        // Object Storage에 파일 업로드
-        try(InputStream fileInputStream = multipartFile.getInputStream()) {
-            ObjectMetadata objectMetadata = new ObjectMetadata();
-            objectMetadata.setContentType(multipartFile.getContentType());
-
-            PutObjectRequest putObjectRequest = new PutObjectRequest(
-                    bucketName,
-                    directory + fileName,
-                    fileInputStream,
-                    objectMetadata
-            ).withCannedAcl(CannedAccessControlList.PublicRead);
-
-            s3.putObject(putObjectRequest);
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
-        File uploadFile = new File(directory + fileName);
-        String type = "";
+        String now = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String uuid = UUID.randomUUID().toString();
+        String originalName = multipartFile.getOriginalFilename();
+        String fileName = uuid + "_" + now + "_" + originalName;
+        String key = directory + fileName;
 
         try {
-            type = Files.probeContentType(uploadFile.toPath());
-        } catch(IOException ie) {
-            System.out.println(ie.getMessage());
+            PutObjectRequest putRequest = PutObjectRequest.builder()
+                    .bucket(bucket)
+                    .key(key)
+                    .contentType(multipartFile.getContentType())
+                    .acl("public-read")
+                    .build();
+
+            s3.putObject(putRequest, RequestBody.fromBytes(multipartFile.getBytes()));
+
+        } catch (IOException e) {
+            System.out.println("파일 업로드 실패: " + e.getMessage());
         }
 
-        if(!type.equals("")) {
-            if(type.startsWith("image")) {
-                auctionImageDto.setFiletype("image");
-            } else {
-                auctionImageDto.setFiletype("etc");
-            }
-        } else {
-            auctionImageDto.setFiletype("etc");
-        }
-
-        auctionImageDto.setFilename(fileName);
-        auctionImageDto.setFileoriginname(multipartFile.getOriginalFilename());
-        auctionImageDto.setFilepath(directory);
-        auctionImageDto.setFilesize(multipartFile.getSize());
-
-        return auctionImageDto;
+        AuctionImageDto dto = new AuctionImageDto();
+        dto.setFilename(fileName);
+        dto.setFileoriginname(originalName);
+        dto.setFilepath(directory);
+        dto.setFilesize(multipartFile.getSize());
+        dto.setFiletype(
+                multipartFile.getContentType() != null && multipartFile.getContentType().startsWith("image")
+                        ? "image"
+                        : "etc"
+        );
+        return dto;
     }
 }
---
+++ src/main/java/bibid/config/S3ClientConfig.java
@@ -0,0 +1,48 @@
+package bibid.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+
+import java.net.URI;
+
+@Configuration
+@Getter
+public class S3ClientConfig {
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String accessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String secretKey;
+
+    @Value("${cloud.aws.s3.endpoint}")
+    private String endpoint;
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(accessKey, secretKey)
+                        )
+                )
+                .region(Region.of(region))  // us-east-1 or ap-northeast-2 등 형식만 맞추면 됨
+                .serviceConfiguration(
+                        S3Configuration.builder()
+                                .pathStyleAccessEnabled(true)  // Cloudflare R2는 필수!
+                                .build()
+                )
+                .build();
+    }
+}
